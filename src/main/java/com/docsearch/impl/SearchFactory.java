bb5322fd3749e0cef04ddf0b5155f98b7efd5755
Shridhar Manvi <shridhar.manvi@ignitionone.com>
2018-01-14 18:48:17 -0500

    Refactored factory

--- src/main/java/com/docsearch/impl/SearchFactory.java
+++ src/main/java/com/docsearch/impl/SearchFactory.java
@@ -5,25 +5,29 @@ import com.docsearch.impl.indexed.IndexedDocumentSearch;
 import com.docsearch.impl.regex.RegExDocumentSearch;
 import com.docsearch.impl.simple.SimpleDocumentSearch;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SearchFactory {
+    private static Map<Integer, DocumentSearch> factory;
+
+    static {
+        factory = new HashMap<>();
+        populateFactory();
+    }
+
+    private static void populateFactory() {
+        factory.put(1, new SimpleDocumentSearch());
+        factory.put(2, new RegExDocumentSearch());
+        factory.put(3, new IndexedDocumentSearch());
+    }
 
     public static DocumentSearch getSearchObj(int method) {
-        DocumentSearch searchMethod = null;
-
-        switch (method) {
-            case 1:
-                searchMethod = new SimpleDocumentSearch();
-                break;
-            case 2:
-                searchMethod = new RegExDocumentSearch();
-                break;
-            case 3:
-                searchMethod = new IndexedDocumentSearch();
-                break;
-            default:
-                throw new RuntimeException("Invalid search method!");
+        if (factory.containsKey(method)){
+            return factory.get(method);
+        } else {
+            throw new RuntimeException("Cannot find implementation!");
         }
-        return searchMethod;
     }
 
 }
